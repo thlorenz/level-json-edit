e18b8cdf4193fce72c57e573dcef3adf6e555ea9
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-24 15:20:07 -0400

    more options to create sublevel indexes

--- example/sublevel-indexes.js
+++ example/sublevel-indexes.js
@@ -17,28 +17,41 @@ var dump = require('level-dump')
  * @name exports
  * @function
  * @param sublevels {LevelDB} sublevels
- * @param isIndex {Function} should return true if prefix is for a sublevel that is an index, otherwise false
+ * @param opts {Object} with the following properties: 
+ *  - isIndex {Function} should return true if prefix is for a sublevel that is an index, otherwise false
+ *  - limit {Number} specifies max number of values to read per sublevel index (useful for generating test samples)
+ *  - maxValuesPerIndex {Number} specifies max number of values per indexed value to store (useful for generating test samples)
  * @param cb {Function} called with an error and/or the indexes
  */
-var go = module.exports = function (sublevels, isIndex, cb) {
+var go = module.exports = function (sublevels, opts, cb) {
 
   var indexSublevels = Object.keys(sublevels)
-    .filter(isIndex)
+    .filter(opts.isIndex)
     .map(function (k) { return sublevels[k] })
 
   function createSearch (acc, sublevel, cb) {
-    var sep = sublevel._sep;
+    var sep = sublevel._sep || '\xff';
     var index = acc[sublevel._prefix] = {};
+    var max = opts.maxValuesPerIndex || Infinity;
 
-    dump.keys(sublevel, onkey, onend);
+    sublevel
+      .createReadStream({ 
+          keys: true
+        , values: false
+        , limit: opts.limit || undefined
+        , start: '' 
+        , end: sublevel.sep
+      })
+      .on('data', onkey)
+      .on('end', onend)
 
     function onkey (k) {
       var parts = k.split(sep)
         , idx = parts.shift()
-        , val = parts.join(sep);
+        , val = parts.join(sep)
 
       if (!index[idx]) index[idx] = [];
-      index[idx].push(val);
+      if (index[idx].length < max) index[idx].push(val);
     }
 
     function onend () {
@@ -53,3 +66,35 @@ var go = module.exports = function (sublevels, isIndex, cb) {
     , cb
   )
 };
+
+// Test
+if (typeof window === 'undefined' && !module.parent) {
+  
+  var level    =  require('level')
+    , sublevel =  require('level-sublevel')
+    , path     =  require('path')
+    , fs       =  require('fs')
+    ;
+
+  var db = level(path.join(__dirname, 'store', 'sample.db'));
+  db = sublevel(db);
+
+  var subData = db.sublevel('data-json', { valueEncoding: 'json' })
+    , byLocation = db.sublevel('idx-location')
+    , byVenue = db.sublevel('idx-venue');
+
+
+  var opts = {
+      isIndex: function isIndex (key) {
+        return (/^idx-/).test(key);
+      }
+    , maxValuesPerIndex: 2
+  }
+
+  go(db.sublevels, opts, function (err, res) {
+    if (err) return console.error(err);
+    console.log('done', res);
+    var json = JSON.stringify(res, null, 2);
+    fs.writeFileSync(path.join(__dirname, 'mocks', 'indexes.json'), json, 'utf8');
+  });
+}
