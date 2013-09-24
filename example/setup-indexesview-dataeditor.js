0de563af3141e7b3628dd9c79ede49ced115715e
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-24 17:11:59 -0400

    styling improvements and loading real indexes

--- example/setup-indexesview-dataeditor.js
+++ example/setup-indexesview-dataeditor.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var renderEditor = require('./render-jsoneditor');
+var sublevelIndexes = require('./sublevel-indexes');
 
 var go = module.exports = function (db, opts) {
   
@@ -9,13 +10,11 @@ var go = module.exports = function (db, opts) {
   var indexesViewer = renderEditor({ indexes: 'loading ...' }, 'indexes-viewer', 'view');
   var dataEditor = renderEditor({ click: 'entry from indexes to load data here' }, 'data-editor');
 
-  var sublevelIndexes = require('./sublevel-indexes-mock');
   sublevelIndexes(db.sublevels, opts, function (err, indexes) {
     if (err) return console.error(err);
 
     indexesViewer.set(indexes);
 
-    indexesViewer.expandAll();
     indexesViewer.table.onclick = function (ev) {
       var tgt = ev.target;
       if ((/^value/).test(tgt.getAttribute('class'))) {
